0ca5cb13a5d74d9d2bf3f798a04d66a9c34e6c66
AbdulGuranni <113431851+AbdulGuranni@users.noreply.github.com>
2024-01-09 21:57:30 +0530

    added database operations

---
+++ FileBasedDatabaseOperation/src/constants/DbConstants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class DbConstants {
+    public static final String DB_DRIVER="com.mysql.cj.jdbc.Driver";
+    public static final String CONNECTION_URL="jdbc:mysql://localhost:3306/StudentData";
+    public static final String USERNAME ="root";
+    public static final String PASSWORD ="";
+}
