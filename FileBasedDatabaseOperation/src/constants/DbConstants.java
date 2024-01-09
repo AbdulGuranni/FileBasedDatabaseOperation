97dd30652d1d30e4822442224101b67d9c638683
AbdulGuranni <113431851+AbdulGuranni@users.noreply.github.com>
2024-01-09 22:04:33 +0530

    Delete FileBasedDatabaseOperation directory

--- FileBasedDatabaseOperation/src/constants/DbConstants.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class DbConstants {
-    public static final String DB_DRIVER="com.mysql.cj.jdbc.Driver";
-    public static final String CONNECTION_URL="jdbc:mysql://localhost:3306/StudentData";
-    public static final String USERNAME ="root";
-    public static final String PASSWORD ="";
-}
